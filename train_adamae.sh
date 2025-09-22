3696d21b61e99b18ed2055e379b55eb542ffac7c
nerdlab53 <retr0sushi.04@gmail.com>
2025-09-22 13:01:34 +0530

    added wandb support

---
+++ train_adamae.sh
@@ -0,0 +1,8 @@
+env_name=Breakout
+python -u train.py \
+    -n "${env_name}-life_done-wm_2L512D8H-25k-seed1" \
+    -seed 1 \
+    -config_path "config_files/STORM_adamae.yaml" \
+    -env_name "ALE/${env_name}-v5" \
+    -trajectory_path "D_TRAJ/${env_name}.pkl" 
+    --use_adamae
